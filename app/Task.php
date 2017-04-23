1436e204417d2b6b38a13d6cd3efb3a528e46679
marczak <marczak1234@gmail.com>
2017-04-23 22:50:57 +0200

    excel report added

--- app/Task.php
+++ app/Task.php
@@ -11,7 +11,45 @@ class Task extends Model
 
     public function user_task()
     {
-        return $this->haveMany('App\UserTask');
+        return $this->hasMany('App\UserTask');
+    }
+
+    public function exportToReport()
+    {
+
+        $ret = [
+            'Nagid' => $this->order_number,
+            'Linid' => $this->line_id,
+            'Data' => $this->date_add,
+            'DataSprz' => $this->date_order,
+            'Logo' => $this->client,
+            'LogoH' => $this->employee,
+            'Priorytet' => $this->prio,
+            'Status' => $this->status,
+            'SymKar' => $this->productID,
+            'Ilosc' => $this->eq,
+            'StTrudnosci' => $this->min_lvl,
+            'Rd' => $this->type,
+            'id' =>$this->id
+        ];
+
+        $times = ['GrawerniaCzas' => 0, 'GrafikaCzas' => 0];
+
+        $user_tasks = $this->user_task;
+//        echo "<pre>";
+//        print_r($user_tasks);
+//        dd($user_tasks);
+        if (!empty($user_tasks)) {
+            foreach ($user_tasks as $task) {
+                if ($task->section == 'grafika')
+                    $times['GrafikaCzas'] += $task->work_time;
+                else if ($task->section == 'grawernia')
+                    $times['GrawerniaCzas'] += $task->work_time;
+            }
+        }
+
+        $ret = array_merge($ret, $times);
+        return $ret;
     }
 
 }
